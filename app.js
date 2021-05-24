188fc020a67c0b37705399ff0ff08f22531131f3
shelleyworrall <79868678+shelleyworrall@users.noreply.github.com>
2021-05-23 19:35:47 -0700

    Add files via upload
    Added days of the week for the weather forecast. Styled the days of the week.

--- app.js
+++ app.js
@@ -23,6 +23,33 @@ function formatDate(timestamp) {
   let day = days[date.getDay()];
   return `${day}${hours}:${minutes}`;
 }
+function displayForecast() {
+  let forecastElement= document.querySelector("#forecast");
+  let forecastHTML= `<div class="row>`; 
+  let days=["Thu", "Fri", "Sat"];
+  days.forEach(function(day){
+
+  })
+forecastHTML =
+  forecastHTML +
+  `
+   <div class="col-2">
+            <div class="weather-forecast-date">${day}</div>
+            <img src="http://openweathermap.org/im/wn/50d@2x.png"
+            alt=""
+            width="42"
+            />
+            <div class="weather-forecast-temperature"></div>
+            <span class="weather-forecast-temperature-max">18º</span>
+            <span class="weather-forecast-temperatire-min">12º</span>
+            
+            </div>
+          </div>
+        `;
+        forecastHTML = forecastHTML + `</div>`;
+        forecastElement.innerHTML= forecastHTML;
+
+}
 function formatDay(timestamp) {
   let date = new Date(timestamp * 1000);
   let day = date.getDay();
@@ -82,6 +109,7 @@ function displayCelsiusTemperature(event) {
 
 let celsiusTemperature = "null";
 
+
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", handleSubmit);
 
@@ -92,7 +120,7 @@ let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
 search("New York");
-
+displayForecast();
 
 
 
