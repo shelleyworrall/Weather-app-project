50072390b9e6c29a3e66d59fd68a08e70ab51f00
shelleyworrall <79868678+shelleyworrall@users.noreply.github.com>
2021-05-23 20:22:50 -0700

    Add files via upload
    Daily weather API and displaying the forecast.

--- app.js
+++ app.js
@@ -23,13 +23,15 @@ function formatDate(timestamp) {
   let day = days[date.getDay()];
   return `${day}${hours}:${minutes}`;
 }
-function displayForecast() {
-  let forecastElement= document.querySelector("#forecast");
-  let forecastHTML= `<div class="row>`; 
-  let days=["Thu", "Fri", "Sat"];
-  days.forEach(function(day){
+function displayForecast(response) {
+  console.log(response.data.daily);
 
-  })
+  let forecastElement= document.querySelector("#forecast");
+  let days=["Thu", "Fri", "Sat", "Sun"];
+
+  let forecastHTML= `<div class="row">`; 
+  
+  days.forEach(function(day) {
 forecastHTML =
   forecastHTML +
   `
@@ -48,7 +50,14 @@ forecastHTML =
         `;
         forecastHTML = forecastHTML + `</div>`;
         forecastElement.innerHTML= forecastHTML;
-
+}
+function getForecast(coordinates) {
+  console.log(coordinates);
+  let apiKey="227a999c611b3441effc132333613ce"
+  let apiUrl=`https://api.openweathermap.org/data/2.5/onecall?
+  lat=${coordinates.lat}&lon=${coordinates.lon}&apid=${apiKey}&units=metric`;
+  
+  axios.get(apiUrl).then(displayForecast);
 }
 function formatDay(timestamp) {
   let date = new Date(timestamp * 1000);
@@ -78,6 +87,8 @@ function displayTemperature(response) {
     `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
+  getForecast(response.data.coord);
+
 }
 function search(city) {
   let apiKey = "227a999c611b3441effc132333613cec";
