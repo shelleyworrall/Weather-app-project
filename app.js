5c30e9137988cadb929723e029bce37c5ffde90c
shelleyworrall <79868678+shelleyworrall@users.noreply.github.com>
2021-05-23 21:16:10 -0700

    Add files via upload

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ function formatDate(timestamp) {
   return `${day}${hours}:${minutes}`;
 }
 
-funcation formatDay(timestamp) {
+function formatDay(timestamp) {
   let date = new Date(timestamp * 1000);
   let day= date.getDay():
   let days= ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
