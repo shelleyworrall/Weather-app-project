d81bc3c0a06bff90016689eebadb405fb2bfb6f8
shelleyworrall <79868678+shelleyworrall@users.noreply.github.com>
2021-05-23 21:21:53 -0700

    Add files via upload
    Corrected a semicolon.

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ function formatDate(timestamp) {
 
 function formatDay(timestamp) {
   let date = new Date(timestamp * 1000);
-  let day= date.getDay():
+  let day= date.getDay();
   let days= ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
   return days[day];
 }
