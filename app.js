ecaac62000d71e5c97f3dcde261e234787888dc0
shelleyworrall <79868678+shelleyworrall@users.noreply.github.com>
2021-05-24 20:47:28 -0700

    Add files via upload
    I went over code and fixed spelling mistakes and missed semicolons.

--- app.js
+++ app.js
@@ -33,25 +33,24 @@ function formatDay(timestamp) {
 function displayForecast(response) {
   let forecast= response.data.daily;
 
-
   let forecastElement= document.querySelector("#forecast");
 
   let forecastHTML= `<div class="row">`; 
   forecast.forEach(function(forecastDay, index) {
     if (index < 6) {
-forecastHTML =
+  forecastHTML =
   forecastHTML +
   `
    <div class="col-2">
             <div class="weather-forecast-date">${formatDay(forecastDay.dt)}</div>
             
-            <img src="http://openweathermap.org/im/wn/${forecastDay.weather[0].icon}@2x.png"
-            alt=""
+            <img src="http://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png"
+            alt= ""
             width="42"
             />
-            <div class="weather-forecast-temperatures"></div>
+            <div class="weather-forecast-temperatures">
             <span class="weather-forecast-temperature-max"> ${Math.round(forecastDay.temp.max)}º</span>
-            <span class="weather-forecast-temperatire-min">${Math.round(forecastDay.temp.min)}º</span>
+            <span class="weather-forecast-temperature-min">${Math.round(forecastDay.temp.min)}º</span>
             
             </div>
           </div>
@@ -62,10 +61,10 @@ forecastHTML =
         forecastElement.innerHTML= forecastHTML;
 }
 function getForecast(coordinates) {
-  console.log(coordinates);
-  let apiKey="227a999c611b3441effc132333613ce"
+  
+  let apiKey="227a999c611b3441effc132333613ce";
   let apiUrl=`https://api.openweathermap.org/data/2.5/onecall?
-  lat=${coordinates.lat}&lon=${coordinates.lon}&apid=${apiKey}&units=metric`;
+  lat=${coordinates.lat}&lon=${coordinates.lon}&apid=${apiKey}&units=imperial`;
   
   axios.get(apiUrl).then(displayForecast);
 }
