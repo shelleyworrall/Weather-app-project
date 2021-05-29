e0381317796297a8557bc85c936a904d8bdd1d26
shelleyworrall <79868678+shelleyworrall@users.noreply.github.com>
2021-05-28 19:01:12 -0700

    Add files via upload
    Changed apiUrl added latitude and longitude.

--- app.js
+++ app.js
@@ -35,16 +35,15 @@ function displayForecast(response) {
 
   let forecastHTML=`<div class="row>`;
   forecast.forEach(function (forecastDay, index) {
-    if (index<6) {
+    if (index < 6) {
       forecastHTML =
       forecastHTML +
       `
       <div class="col-2">
       <div class="weather-forecast-date">${formatDay(forecastDay.dt)}</div>
-      <img src="http://openweathermap.org/img/wn/${forecastDay.weather[0].icon}
-      2x.png" alt="" width="42"/>
+      <img src="http://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png" alt="" width="42"/>
 <div class="weather-forecast-temperatures">
-<span class="weather-forecast-temperatures-max">${Math.round(forecastDay.temp.max)}º </span>
+<span class="weather-forecast-temperatures-max">${Math.round(forecastDay.temp.max)} º</span>
 <span class="weather-forecast-temperature-min"> ${Math.round(forecastDay.temp.min)} º</spam>
 </div>
 </div>
@@ -56,8 +55,7 @@ function displayForecast(response) {
 }
 function getForecast(coordinates) {
   let apiKey = "227a999c611b3441effc132333613cec";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayForecast);
 }
 
